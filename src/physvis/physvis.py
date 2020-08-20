dc109f61fec5cd7ad5012f361f15b786da3d1b49
David Verweij <davidverweij@gmail.com>
2020-08-20 12:11:12 +0200

    cleans code, saves combined .csv

--- src/physvis/physvis.py
+++ src/physvis/physvis.py
@@ -4,6 +4,19 @@ import re
 import pandas as pd
 
 
+def create_output_folder(output_path: str) -> Path:
+    """Creates a path to store output data if it does not exists.
+    Args:
+        path: the path from user in any format (relative, absolute, etc.)
+    Returns:
+        A path to store output data.
+    """
+    path = Path(output_path)
+    if not path.exists():
+        path.mkdir(parents=True, exist_ok=True)
+    return path
+
+
 def convert(
     input: str = "input", output: str = "output", delimiter: str = ";"
 ) -> None:
@@ -12,42 +25,37 @@ def convert(
     all_files = list(Path(input).glob('*.csv'));
 
     li = []
+    naming_columns = ['particip','phys','partic_orien','cond']
 
     for filename in all_files:
-        print(filename.stem)
-        df = pd.read_csv(filename, index_col=None, header=0, delimiter=delimiter, keep_default_na=False)
-        li.append(df)
+        ''' split the filename into columns
+        Expecting filesnames in the format PX_0_N_0
+            Participant = [P1-P20]
+            Phys = [1-6]
+            Orientation = [N, E, S, W]
+            Condition = [0-2]
+                0 = clustering
+                1 = single move
+                2 = multiple moves
+        '''
+        df1 = pd.read_csv(filename, index_col=None, header=0, delimiter=delimiter, keep_default_na=False)
+
+        # removed empty (or in our case unnamed) columns
+        df1 = df1.loc[:, ~df1.columns.str.contains('^Unnamed')]
+
+        # split the coordinates in two columns, and remove original
+        df1[['cube_x','cube_y']] = df1['coordinates'].str.split(pat=',',expand=True)
+        df1 = df1.drop(columns=['coordinates'])
+
+        # prepend the filname data to all rows
+        df2 = pd.DataFrame( [filename.stem.split(sep='_')]*len(df1.index) ,columns=naming_columns)
+
+        # remove the 'P' before participant
+        df2['particip']= df2['particip'].str.lstrip('P')
+
+
+        li.append(pd.concat([df2,df1],axis=1))
 
     frame = pd.concat(li, axis=0, ignore_index=True)
-    print(frame.head(20))
-
-    '''
-    with open(data, "rt") as csvfile:
-        csvdict = csv.DictReader(csvfile, delimiter=delimiter)
-        csv_headers = csvdict.fieldnames
-        if csv_headers and name not in csv_headers:
-            print("Column name not found. Please enter valid column name")
-            exit(1)
-        docx = MailMerge(template)
-        docx_mergefields = docx.get_merge_fields()
-
-        print(f"DOCX fields : {docx_mergefields}")
-        print(f"CSV field   : {csv_headers}")
-
-        # see if all fields are accounted for in the .csv header
-        column_in_data = set(docx_mergefields) - set(csv_headers)
-        if len(column_in_data) > 0:
-            print(f"{column_in_data} is in the word document, but not csv.")
-            return
-
-        print("All fields are present in your csv. Generating Word docs ...")
-        output_path = create_output_folder(path)
-
-        for row in csvdict:
-            # Must create a new MailMerge for each file
-            docx = MailMerge(template)
-            single_document = {key: row[key] for key in docx_mergefields}
-            docx.merge_templates([single_document], separator="page_break")
-            filename = create_unique_name(row[name], output_path)
-            docx.write(filename)
-    '''
+    print(frame.info());
+    frame.to_csv(path_or_buf=create_output_folder(output) / 'combined.csv', sep=';', header=True)
