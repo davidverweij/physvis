61477d7155f0541efebbb73656ebb864bc42bea4
David Verweij <davidverweij@gmail.com>
2020-09-07 14:01:39 +0100

    setup for first calc method

--- src/physvis/console.py
+++ src/physvis/console.py
@@ -10,6 +10,9 @@ from . import maths
 @click.version_option(version=__version__)
 @click.option("--input", "-i", default="output/combined.csv", help="The location of the large .csv file compiled by collect(). Default is 'output/combined.csv'")
 def main(input: str, output: str, delimiter: str, save: bool) -> None:
+
+    click.echo(f"Press 'control+c' to abort this program at any point.\n")
+
     click.echo(f"Collecting .csv files from {input}")
     interactions.collect(input, output, delimiter, save = True)
     click.echo("Done.")
@@ -19,6 +22,8 @@ def main(input: str, output: str, delimiter: str, save: bool) -> None:
 @click.option("--delimiter", "-d", default=";", help="Delimiter used in your csv files. Default is ';'")
 @click.option("--input", "-i", default="output/combined.csv", help="The location of the large .csv file compiled by collect(). Default is 'output/combined.csv'")
 def vis(input: str, delimiter: str) -> None:
+    click.echo(f"Press 'control+c' to abort this program at any point.\n")
+
     click.echo(f"\nWhich situation do you want to visualise?")
     frame = helpers.get_large_csv(input, delimiter)
     # print(frame.info())
@@ -53,6 +58,8 @@ def vis(input: str, delimiter: str) -> None:
 @click.option("--delimiter", "-d", default=";", help="Delimiter used in your csv files. Default is ';'")
 @click.option("--input", "-i", default="input", help="The input location of the .csv files. Default is 'input'")
 def collect(input: str, delimiter: str) -> None:
+    click.echo(f"Press 'control+c' to abort this program at any point.\n")
+
     click.echo(f"Collecting Data from {input}")
     frame = interactions.generate_large_csv(input = input, output = 'output', delimiter = delimiter, save = True)
     click.echo(f"See the output/combined.csv for the combined result")
@@ -62,8 +69,10 @@ def collect(input: str, delimiter: str) -> None:
 @click.option("--delimiter", "-d", default=";", help="Delimiter used in your csv files. Default is ';'")
 @click.option("--input", "-i", default="output/combined.csv", help="The location of the large .csv file compiled by collect(). Default is 'output/combined.csv'")
 def calc(input: str, delimiter: str) -> None:
-    click.echo(f"\nWhich calculation do you want to perform?")
-    # frame = helpers.get_large_csv(input, delimiter)
+    click.echo(f"Press 'control+c' to abort this program at any point.")
+
+    click.echo(f"\nWhich calculation do you want to perform?\n")
+    frame = helpers.get_large_csv(input, delimiter)
     # print(frame.info())
 
     repeat = True
@@ -72,25 +81,29 @@ def calc(input: str, delimiter: str) -> None:
         repeat = False
 
         # get user input
-        functions = [x[0] for x in inspect.getmembers(maths, inspect.isfunction)]
-        print(functions)
-        calculation = click.prompt("  Participant", type=click.Choice(functions, case_sensitive=False))
+        possible_functions = [x[0] for x in inspect.getmembers(maths, inspect.isfunction)]
+        possible_functions.reverse()
+        show_options = [str(i) + '. ' + x for i, x in enumerate(possible_functions, 1)]
+        print("\n".join(show_options) + "\n")
 
-        # run the chosen method, with options: getattr(maths, calculation)(**options)
-        getattr(maths, calculation)()
-        '''
+        chosen_function_nr = click.prompt("  Calulation", type=click.Choice([str(x) for x in range(1,len(possible_functions)+1)]))
+        chosen_function = possible_functions[int(chosen_function_nr)-1]
+
         # display result, or err
         try:
-            click.echo(f"\nI found this situation:\n")
-            interactions.display(frame, participant, condition, orientation, physicalisation)
-            click.echo(f"\nThe visualisation should now open in the browser\n")
+            click.echo(f"\nRunning '{chosen_function}':\n")
+
+            # run the chosen method, with options: getattr(maths, chosen_function)(**options)
+            getattr(maths, chosen_function)(frame)
+
+            click.echo(f"\nFinished calculation.\n")
         except Exception as e:
-            print(f"An error occured in visualising Phys {physicalisation}, for P{participant}_{condition}_{orientation}: {e}")
+            print(f"An error occured in the {chosen_function} calculation: {e}")
 
         # possibly visualise another one
         repeat = False
-        if click.confirm('Would you like to visualise another one?'):
+        if click.confirm('Would you like to conduct another calculation?'):
             repeat = True
-        '''
+
 
     click.echo(f"\nTill next time!\n")
--- src/physvis/maths.py
+++ src/physvis/maths.py
@@ -1,6 +1,31 @@
-def get_stats() -> None:
-    print("Hello World!")
+import pandas as pd
+
+from .helpers import naming_columns as nc
+
+def move_types_overall(frame: pd.DataFrame) -> None:
+    """Calculates various stats about the different moves participants made
+    For 'standard' stats, see https://pandas.pydata.org/pandas-docs/stable/user_guide/groupby.html#aggregation
+    Here, we use our own aggregation method, see https://pandas.pydata.org/pandas-docs/stable/user_guide/groupby.html#flexible-apply
+    Args:
+        frame: the data frame storing data to be rendered
+    Returns:
+        nothing
+    """
+
+    # method local to this method
+    def _custom_calc(x):
+        pass
+
+    for name, group in frame.groupby(nc[0:2]):
+        group.apply(_custom_calc)
+
+
+
+
+'''
+def move_consistency() -> None:
     pass
 
-def get_cluster_stats() -> None:
+def cluster_change_type() -> None:
     pass
+'''
